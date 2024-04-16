16a6728be525eb63152147d2487d227194693c90
Taigg275 <taigg27052003@gmail.com>
2024-04-16 21:52:25 +0700

    final code
    shopi

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/clone_shopee');
+        await mongoose.connect(
+            'mongodb+srv://taigg27052003:123@cluster0.2kldgnx.mongodb.net/clone_shopee?retryWrites=true&w=majority&appName=Cluster0',
+        );
 
         console.log('Thanh cong');
     } catch (error) {
