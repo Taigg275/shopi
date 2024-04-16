a59ae4718533a879a0ca6018d4f77c931c133b6d
Taigg275 <137408352+Taigg275@users.noreply.github.com>
2024-04-16 22:06:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,36 +7,32 @@ import cors from 'cors';
 import categoryRoutes from './routes/categoryRoute.js';
 import productRoutes from './routes/productRoute.js';
 import path from 'path';
-import {fileURLToPath} from 'url';
-//configure env
+
+// Configure environment variables
 dotenv.config();
 
-//database config
+// Database config
 connectDB();
 
-
-const __filename = fileURLToPath(import.meta.path);
-const __dirname  = path.dirname(__filename)
-
-//rest object
+// Rest object
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(process.cwd(), 'client', 'build')));
 
-//routes
+// Routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
 app.use('*', function (req, res) {
-    req.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(process.cwd(), 'client', 'build', 'index.html'));
 });
 
-//rest api
+// Rest API
 app.get('/', (_req, res) => {
     res.send('<h1>Hello</h1>');
 });
